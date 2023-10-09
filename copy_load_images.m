d751a43c45b5a5a10e24dcc21c51170bd41998e2
Jan Ferbr <74875970+ferbrjan@users.noreply.github.com>
2023-10-09 09:42:43 +0200

    Delete copy_load_images.m

--- copy_load_images.m
+++
@@ -1,8 +0,0 @@
-function [rgb, depth_ios, depth_mvs] = copy_load_images(root_dir, image_name)
-    rgb = imread(fullfile(root_dir, 'images1', image_name));
-    load(fullfile(root_dir, 'depth1', strrep(image_name,'jpg','mat')));
-    depth_ios = depth;
-    load(fullfile(root_dir, 'depth2', strrep(sprintf('%s%s', '', image_name),'jpg','mat')));
-    depth_mvs = depth;
-end
-
