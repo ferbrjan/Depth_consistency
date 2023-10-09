f2cea749a17838798bb8dee8a8c79d27e1150415
Jan Ferbr <74875970+ferbrjan@users.noreply.github.com>
2023-10-09 09:42:58 +0200

    Delete generate_test_img.m

--- generate_test_img.m
+++
@@ -1,9 +0,0 @@
-% WARNING THIS TAKES A S***LOAD OF TIME AND STORAGE!!
-
-load("00180.mat")
-im_w = 1920;
-im_h = 1440;
-%% 
-data = cellfun(@softmax_count,GTs,'uniformoutput',false);
-%%
-save("00180_test.mat","data",'-v7.3')
