6bf154b41d3ba1dd30cc77cfbb9b7e642206f369
Jan Ferbr <74875970+ferbrjan@users.noreply.github.com>
2023-10-09 09:43:17 +0200

    Delete load_images.m

--- load_images.m
+++
@@ -1,8 +0,0 @@
-function [rgb, depth_ios, depth_mvs] = load_images(root_dir, image_name)
-    rgb = imread(fullfile(root_dir, 'images', image_name));
-    load(fullfile(root_dir, 'depth_ios', strrep(image_name,'jpg','mat')));
-    depth_ios = depth;
-    load(fullfile(root_dir, 'depth_mvs', strrep(sprintf('%s%s', '000', image_name),'jpg','mat')));
-    depth_mvs = squeeze(depth_mvs);
-end
-
